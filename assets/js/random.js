a73b8e4f36abb962fe6d31fa987977f0aa9bfb3f
Valentin <59359218+ChristopherStuff@users.noreply.github.com>
2020-07-14 21:34:01 +0300

    Add files via upload

---
+++ assets/js/random.js
@@ -0,0 +1,43 @@
+var options = {
+    loop: true,
+    typeSpeed: 60,
+    backSpeed: 70,
+    backDelay: 900,
+    showCursor: true,
+    startDelay: 90,
+    strings: ["Сделаем их вместе!", "Кандидат - Кристофер Квинзи", "Голосуй за своё будущее - Слуга Народа!", "Не дай им сломать нас!"]
+}
+var typed = new Typed(".commands", options);
+
+var options1 = {
+    loop: true,
+    typeSpeed: 60,
+    backSpeed: 70,
+    backDelay: 900,
+    showCursor: true,
+    startDelay: 90,
+    strings: ["Наше Руководство"]
+}
+var typed2 = new Typed(".command", options1);
+
+var options2 = {
+    loop: true,
+    typeSpeed: 60,
+    backSpeed: 70,
+    backDelay: 900,
+    showCursor: true,
+    startDelay: 90,
+    strings: ["Наша Программа"]
+}
+var typed3 = new Typed(".comman", options2);
+
+var options3 = {
+    loop: true,
+    typeSpeed: 60,
+    backSpeed: 70,
+    backDelay: 900,
+    showCursor: true,
+    startDelay: 90,
+    strings: ["Присоединяйся к нам! Мы ждём именно тебя!"]
+}
+var typed4 = new Typed(".comma", options3);
